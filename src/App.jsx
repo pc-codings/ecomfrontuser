e60b27f0b1a72b5212f11045e94b0ea8fc9d99db
paritosh <paritosh.chowdhury@alltrak.co>
2025-07-07 18:51:58 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Verify from './pages/Verify'
 
-function App() {
 
+function App() {
 
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
